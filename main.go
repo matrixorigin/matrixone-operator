5398038b3b1b161d92f86caa5a3e69c69cb207e7
wanglei <wanglei4687@gmail.com>
2021-12-09 16:51:57 +0800

    add log

--- main.go
+++ main.go
@@ -80,6 +80,7 @@ func main() {
 
 	if err = (&controllers.MatrixoneReconciler{
 		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("Matrixone"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Matrixone")
