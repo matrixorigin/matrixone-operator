28fe75b58f03c1069990b5cb051d5836c5bcdfec
wanglei <74483764+wanglei4687@users.noreply.github.com>
2021-12-23 15:39:20 +0800

    init project struct

---
+++ api/matrixone/v1alpha1/groupversion_info.go
@@ -0,0 +1,20 @@
+// Package v1alpha1 contains API Schema definitions for the matrixorigin.cn v1alpha1 API group
+//+kubebuilder:object:generate=true
+//+groupName=matrixorigin.cn
+package v1alpha1
+
+import (
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"sigs.k8s.io/controller-runtime/pkg/scheme"
+)
+
+var (
+	// GroupVersion is group version used to register these objects
+	GroupVersion = schema.GroupVersion{Group: "matrixorigin.cn", Version: "v1alpha1"}
+
+	// SchemeBuilder is used to add go types to the GroupVersionKind scheme
+	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
+
+	// AddToScheme adds the types in this group-version to the given scheme.
+	AddToScheme = SchemeBuilder.AddToScheme
+)
