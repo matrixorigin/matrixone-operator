8c59d4060cd98a0b15762eaebe622e558f30eb6c
wanglei <74483764+wanglei4687@users.noreply.github.com>
2021-12-27 14:20:18 +0800

    add config

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-#  Matrixone Operator
+# Matrixone Operator
 
+[![LICENSE](https://img.shields.io/github/license/pingcap/tidb-operator.svg)](https://github.com/pingcap/tidb-operator/blob/master/LICENSE)
+[![Language](https://img.shields.io/badge/Language-Go-blue.svg)](https://golang.org/)
--- main.go
+++ main.go
@@ -51,9 +51,9 @@ func init() {
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
-	var probeAddr string
+	// var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
+	// flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
@@ -66,12 +66,12 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Scheme:                 scheme,
-		MetricsBindAddress:     metricsAddr,
-		Port:                   9443,
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "0c8ab548.matrixorigin.cn",
+		Scheme:             scheme,
+		MetricsBindAddress: metricsAddr,
+		Port:               9443,
+		// HealthProbeBindAddress: probeAddr,
+		LeaderElection:   enableLeaderElection,
+		LeaderElectionID: "0c8ab548.matrixorigin.cn",
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
