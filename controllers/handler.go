66f878f9118c2eae95dd18a88809b5f1991da430
Lei <wanglei4687@gmail.com>
2021-12-30 00:29:36 +0800

    fix struct toml

--- controllers/handler.go
+++ controllers/handler.go
@@ -1,15 +1,7 @@
 package controllers
 
 import (
-	matrixonev1alpha1 "github.com/matrixorigin/matrixone-operator/api/v1"
-
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var logger = logf.Log.WithName("matrixone_operator_handler")
-
-func deployMatrixoneCluster(moc *matrixonev1alpha1.MatrixoneCluster) error {
-	logger.Info("deployMatrixoneCluster")
-
-	return nil
-}
