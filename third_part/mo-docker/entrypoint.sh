425ea268127f9d4ad7ced68d758db03a47bf83cc
lei <wanglei4687@gmail.com>
2022-04-24 13:57:39 +0800

    update image to kc-0.3.0

--- third_part/mo-docker/entrypoint.sh
+++ third_part/mo-docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 reg="-[0]"
 host="${POD_NAME}.${SERVICE_NAME}"
@@ -6,11 +6,11 @@ host="${POD_NAME}.${SERVICE_NAME}"
 if [[ $host =~ $reg ]];
 then
     sed  -i "s/\"JOIN\"/\"\"/g" system_vars_config.toml
-else 
+else
     sed -i "s/\"JOIN\"/\"http\:\/\/${FIRST_NODE}\:40000\"/g" system_vars_config.toml
 fi
 
 sed -i "s/HOST/$host/g" system_vars_config.toml
 sed -i "s/NAME/${POD_NAME}/g" system_vars_config.toml
 
-/usr/local/bin/dumb-init ./mo-server ./system_vars_config.toml
+./mo-server ./system_vars_config.toml
