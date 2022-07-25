4aaf6b484b5a594aedecc8ea8d14d64980fd0275
wanglei4687 <wanglei4687@gmail.com>
2022-07-25 14:19:42 +0800

    feat: init dnset controller
    Signed-off-by: wanglei4687 <wanglei4687@gmail.com>

--- dnset/dn_controller.go
+++ dnset/dn_controller.go
@@ -1,27 +1,54 @@
 package dnset
 
 import (
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/tools/record"
-	ctrl "sigs.k8s.io/controller-runtime"
-	logf "sigs.k8s.io/controller-runtime/pkg/log"
-	"sigs.k8s.io/controller-runtime/pkg/client"
+	"github.com/matrixorigin/matrixone-operator/api/core/v1alpha1"
+	recon "github.com/matrixorigin/matrixone-operator/runtime/pkg/reconciler"
+	kruisev1alpha1 "github.com/openkruise/kruise/apis/apps/v1alpha1"
 )
 
-const (
-	controllerName = "DNSetController"
-)
-
-var log = logf.Log.WithName("DNSet-controller")
-
-type  DNReconcilerConfig struct {}
-
-type DNReconciler struct {
-	client.Client
-	scheme *runtime.Scheme
-	recoder record.EventRecorder
+type DNSetActor struct{}
+type WithResources struct{
+	*DNSetActor
+	cloneSet *kruisev1alpha1.CloneSet
 }
 
-func NewReconciler(mgr ctrl.Manager, config DNReconcilerConfig) {
+var _ recon.Actor[*v1alpha1.DNSet] = &DNSetActor{}
 
+func (r *DNSetActor) with(cs *kruisev1alpha1.CloneSet) *WithResources {
+	return &WithResources{DNSetActor: r, cloneSet: cs}
+}
+
+// Observe: observe dnset bootstrap
+func (d *DNSetActor) Observe(ctx *recon.Context[*v1alpha1.DNSet]) (recon.Action[*v1alpha1.DNSet], error) {
+	return nil, nil
+}
+
+// Create: create dn pod
+func (d *DNSetActor) Create(ctx *recon.Context[*v1alpha1.DNSet]) error {
+	return nil
+}
+
+// Finalize: finalize dnset
+func (d *DNSetActor) Finalize(ctx *recon.Context[*v1alpha1.DNSet]) (bool, error) {
+	return true, nil
+}
+
+// Bootstrap: bootstrap dnset
+func (d *DNSetActor) Bootstrap(ctx *recon.Context[*v1alpha1.DNSet]) error {
+	return nil
+}
+
+// Scale: scale in/scale out dnset
+func (w *WithResources) Scale(ctx *recon.Context[*v1alpha1.DNSet]) error {
+	return nil
+}
+
+// Repair: repair dnset
+func (w *WithResources) Repair(ctx *recon.Context[*v1alpha1.DNSet]) error {
+	return nil
+}
+
+// Update: update dnset
+func (w *WithResources) Update(ctx *recon.Context[*v1alpha1.DNSet]) error {
+	return nil
 }
