69bbbe3d6d2a505822f070fb537ecafbff10b2e3
Aylei <rayingecho@gmail.com>
2022-09-07 11:44:30 +0800

    fix: update license
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- hack/update-license.sh
+++ hack/update-license.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 cd ${ROOT}
 
-for i in $(find -name \*.go); do
+for i in $(find . -name \*.go); do
   if ! grep -q Copyright $i
   then
     cat ./hack/boilerplate.go.txt $i >$i.new && mv $i.new $i
