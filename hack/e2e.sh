08ffd6b2fe7a10c4e883af6272e54dea4a10b00a
wanglei <wanglei4687@gmail.com>
2022-10-10 22:51:14 +0800

    feat: add deploy script (#233)
    * feat: add deploy script
    
    Signed-off-by: wanglei4687 <wanglei4687@gmail.com>

---
+++ hack/e2e.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+#set -euo pipefail
+
+OPNAMESPACE=${OPNAMESPACE:-"mo-system"}
+
+function e2e::check() {
+  CMD=pgrep
+  PPROC=ginkgo
+  crds=$(kubectl get crds --no-headers=true | awk '/matrixorigin/{print $1}')
+  echo "> E2E check"
+  if [ -n "`$CMD $PPROC`" ]; then
+    echo "Already running e2e test, Wait for E2E Ready or Kill it"
+    exit 1
+  elif [[ $crds != "" ]]; then
+    echo "Please delete old CRDS"
+    exit 1
+  else
+    echo "Can run e2e test"
+  fi
+}
+
+function e2e::run() {
+    echo "> Run e2e test"
+    make ginkgo
+    ./bin/ginkgo -stream -slowSpecThreshold=3000 ./test/e2e/... -- \
+                -mo-version="${MO_VERSION}" \
+                -mo-image-repo="${MO_IMAGE_REPO}"
+
+}
+
+function e2e::install() {
+  echo "> Create operator namespace"
+  kubectl create ns "${OPNAMESPACE}"
+  echo "> Install mo operator"
+  helm install mo ./charts/matrixone-operator --dependency-update --set image.repository=${REPO} --set image.tag=${TAG} -n "${OPNAMESPACE}"
+
+  echo "> Wait webhook certificate injected"
+  sleep 30
+}
+
+function e2e::deleteFinalizers() {
+    echo "> Delete resources"
+    ns=$(kubectl get ns --all-namespaces --no-headers=true | awk '/^e2e/{print $1}')
+
+    for value in $ns
+    do
+        echo "namespace: $value"
+        kubectl patch pod log-log-0 --type='json' -p='[{"op":"remove", "path":"/metadata/finalizers", "values":["matrixorigin.io/confirm-deletion"]}]' -n $value
+    done
+
+}
+
+function e2e::cleanup() {
+    e2e::deleteFinalizers
+    echo "Delete e2e test namespace"
+    kubectl get ns --all-namespaces --no-headers=true | awk '/^e2e/{print $1}' | xargs kubectl delete ns
+    # Uninstall helm charts
+    echo "Uninstall helm charts..."
+    helm uninstall mo -n "${OPNAMESPACE}"
+    echo "Wait for charts uninstall"
+    sleep 10
+    echo "Delete operator namespace"
+    kubectl delete ns "$OPNAMESPACE"
+}
+
+function e2e::workflow() {
+  trap "e2e::cleanup" EXIT
+  e2e::check
+  e2e::install
+  e2e::run
+}
+
+function e2e::start() {
+  echo "> Start e2e workflow"
+  e2e::workflow
+}
+
+e2e::start
--- hack/kind-e2e.sh
+++ hack/kind-e2e.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+CLUSTER=${CLUSTER:-"mo"}
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 cd ${ROOT}
 source ${ROOT}/hack/lib.sh
@@ -14,25 +15,28 @@ function e2e::prepare_image() {
     kind load docker-image --name ${1} ${2}
 }
 
-function e2e::kubectl_wait_appear() {
-    local WAIT_N=0
-    local MAX_WAIT=5
-    while true; do
-        kubectl get $@ 2>/dev/null | grep NAME && break
-        if [ ${WAIT_N} -lt ${MAX_WAIT} ]; then
-            WAIT_N=$((WAIT_N+1))
-            echo "Waiting for $@ to be created, sleeping for ${WAIT_N} seconds"
-            sleep ${WAIT_N}
-        else
-            echo "Timeout waiting for $@"
-            exit 1
-        fi
-    done
+function e2e::kind-cleanup() {
+    echo "> Tearing down"
+    kind delete cluster --name "${1}"
 }
 
-function e2e::cleanup() {
-    echo "> Tearing down"
-    kind delete cluster --name ${1}
+function e2e::ensure-kind() {
+    echo "> Create kind cluster"
+    export KUBECONFIG=$(mktemp)
+    echo "$KUBECONFIG"
+    kind create cluster --name "${CLUSTER}"
+    kubectl apply -f test/kind-rbac.yml
+    make build
+    kind load docker-image --name "${CLUSTER}" ${REPO}:${TAG}
+
+    echo "> Ensure k8s cluster is ready"
+    kubectl cluster-info
+    kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=30s
+}
+
+function e2e::load_image() {
+  e2e::prepare_image ${CLUSTER} ${MO_IMAGE_REPO}:${MO_VERSION}
+  e2e::prepare_image ${CLUSTER} openkruise/kruise-manager:v1.2.0
 }
 
 if [[ -z ${MO_VERSION+undefined-guard} ]]; then
@@ -44,37 +48,19 @@ hack::ensure_helm
 hack::ensure_kind
 
 CLUSTER=${CLUSTER:-mo}
-MO_IMAGE_REPO=${MO_IMAGE_REPO:-"matrixorigin/matrixone"}
-echo "> Run operator E2E with MO ${MO_IMAGE_REPO}:${MO_VERSION}"
 
-trap "e2e::cleanup ${CLUSTER}" EXIT
 
-echo "> Create kind cluster"
-export KUBECONFIG=$(mktemp)
-echo $KUBECONFIG
-kind create cluster --name ${CLUSTER}
-kubectl apply -f test/kind-rbac.yml
-make build
-kind load docker-image --name ${CLUSTER} matrixorigin/matrixone-operator:latest
+function e2e::kind-e2e() {
+  echo "> Start kind e2e test"
 
-echo "> Prepare e2e images"
-e2e::prepare_image ${CLUSTER} ${MO_IMAGE_REPO}:${MO_VERSION}
-e2e::prepare_image ${CLUSTER} openkruise/kruise-manager:v1.2.0
+  trap "e2e::kind-cleanup ${CLUSTER}" EXIT
+  e2e::ensure-kind
+  e2e::load_image
+  echo "> Run e2e test"
+  bash ./hack/e2e.sh
+}
 
-echo "> Install mo operator"
-helm install mo ./charts/matrixone-operator --dependency-update --set image.tag="latest"
 
-echo "> Ensure k8s cluster is ready"
-kubectl cluster-info
-kubectl wait --for=condition=Ready pods --all --all-namespaces --timeout=30s
+e2e::kind-e2e
+exec "$@"
 
-echo "> Wait webhook certificate injected"
-sleep 30
-
-#if [[ ! -z ${AWS_ACCESS_KEY_ID+undefined-guard} ]] ; then
-#  echo "> Ensure S3 credentials"
-#  kubectl create secret generic aws --from-literal=AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --from-literal=AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-#fi
-
-echo "> Run e2e test"
-$GINKGO -stream -slowSpecThreshold=3000 ./test/e2e/... -- -mo-version=${MO_VERSION} -mo-image-repo=${MO_IMAGE_REPO}
