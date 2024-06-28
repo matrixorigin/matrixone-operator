285ef3c38a830ffebf53c0c65dd14bbced97aabd
cyberchen98 <forsakenhale@gmail.com>
2024-06-28 14:04:14 +0800

    feat: add validation for PodSet and Overlay

--- pkg/webhook/common.go
+++ pkg/webhook/common.go
@@ -15,12 +15,20 @@
 package webhook
 
 import (
+	"fmt"
+	"github.com/blang/semver/v4"
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1validation "k8s.io/apimachinery/pkg/apis/meta/v1/validation"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"k8s.io/apimachinery/pkg/util/validation"
 	"k8s.io/apimachinery/pkg/util/validation/field"
+	"k8s.io/kubernetes/pkg/apis/core"
+	corevalidation "k8s.io/kubernetes/pkg/apis/core/validation"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/matrixorigin/matrixone-operator/api/core/v1alpha1"
+	"github.com/matrixorigin/matrixone-operator/pkg/webhook/convertor"
 )
 
 func invalidOrNil(allErrs field.ErrorList, r client.Object) error {
@@ -43,6 +51,7 @@ func validateMainContainer(c *v1alpha1.MainContainer, parent *field.Path) field.
 	if c.Image == "" {
 		errs = append(errs, field.Invalid(parent.Child("image"), c.Image, "image must be set"))
 	}
+	errs = append(errs, validateResourceRequirements(&c.Resources, parent.Child("resources"))...)
 	return errs
 }
 
@@ -69,4 +78,71 @@ func validateGoMemLimitPercent(memPercent *int, path *field.Path) field.ErrorLis
 		errs = append(errs, field.Invalid(path, memPercent, "memoryLimitPercent value must be in interval (0, 100]"))
 	}
 	return errs
+}
+
+func validatePodSet(podSet *v1alpha1.PodSet, path *field.Path) field.ErrorList {
+	var errs field.ErrorList
+	if podSet.ClusterDomain != "" {
+		errs = append(errs, validation.IsFullyQualifiedDomainName(path.Child("clusterDomain"), podSet.ClusterDomain)...)
+	}
+	if podSet.Overlay != nil {
+		errs = append(errs, validateOverlays(podSet.Overlay, path.Child("overlay"))...)
+	}
+	if podSet.SemanticVersion != nil {
+		_, err := semver.Parse(*podSet.SemanticVersion)
+		if err != nil {
+			errs = append(errs, field.Invalid(path.Child("semanticVersion"), *podSet.SemanticVersion, err.Error()))
+		}
+	}
+	if podSet.ExportToPrometheus != nil && *podSet.ExportToPrometheus {
+		if podSet.PromDiscoveryScheme == nil {
+			errs = append(errs, field.Invalid(path.Child("promDiscoveryScheme"), "", "ExportToPrometheus is enabled but PromDiscoveryScheme is not provided"))
+		} else if *podSet.PromDiscoveryScheme != v1alpha1.PromDiscoverySchemePod && *podSet.PromDiscoveryScheme != v1alpha1.PromDiscoverySchemeService {
+			errs = append(errs, field.Invalid(path.Child("promDiscoveryScheme"), *podSet.PromDiscoveryScheme,
+				fmt.Sprintf("invalid PromDiscoveryScheme, must be %s or %s", v1alpha1.PromDiscoverySchemePod, v1alpha1.PromDiscoverySchemeService)))
+		}
+	}
+
+	errs = append(errs, validateMainContainer(&podSet.MainContainer, path.Child("mainContainer"))...)
+	errs = append(errs, metav1validation.ValidateLabels(podSet.NodeSelector, path.Child("nodeSelector"))...)
+
+	return errs
+}
+
+func validateOverlays(overlay *v1alpha1.Overlay, path *field.Path) field.ErrorList {
+	var errs field.ErrorList
+	errs = append(errs, metav1validation.ValidateLabels(overlay.PodLabels, path.Child("podLabels"))...)
+	errs = append(errs, corevalidation.ValidateAnnotations(overlay.PodAnnotations, path.Child("podAnnotations"))...)
+	if overlay.RuntimeClassName != nil {
+		errs = append(errs, corevalidation.ValidateRuntimeClassName(*overlay.RuntimeClassName, path.Child("runtimeClassName"))...)
+	}
+	errs = append(errs, validateTolerations(overlay.Tolerations, path.Child("tolerations"))...)
+
+	return errs
+}
+
+func validateTolerations(v1tolerations []corev1.Toleration, path *field.Path) field.ErrorList {
+	tolerations, err := convertor.ConvertTolerations(v1tolerations)
+	if err != nil {
+		return field.ErrorList{field.Invalid(path, v1tolerations, err.Error())}
+	}
+
+	return corevalidation.ValidateTolerations(tolerations, path)
+}
+
+func validateResourceRequirements(v1requirements *corev1.ResourceRequirements, path *field.Path) field.ErrorList {
+	requirements, err := convertor.ConvertResourceRequirements(v1requirements)
+	if err != nil {
+		return field.ErrorList{field.Invalid(path, v1requirements, err.Error())}
+	}
+
+	return corevalidation.ValidateResourceRequirements(requirements, makeClaimNameSet(requirements.Claims), path, corevalidation.PodValidationOptions{})
+}
+
+func makeClaimNameSet(claims []core.ResourceClaim) sets.String {
+	claimNames := sets.NewString()
+	for _, claim := range claims {
+		claimNames.Insert(claim.Name)
+	}
+	return claimNames
 }
