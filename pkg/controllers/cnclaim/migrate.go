5c8cd634c6a74684f64a181d5a6c0fc6ae5a0b33
Aylei <rayingecho@gmail.com>
2024-09-04 16:41:11 +0800

    feat: more logs for store draining (#548)
    Signed-off-by: Aylei <rayingecho@gmail.com>

--- pkg/controllers/cnclaim/migrate.go
+++ pkg/controllers/cnclaim/migrate.go
@@ -16,6 +16,8 @@ package cnclaim
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/go-errors/errors"
 	recon "github.com/matrixorigin/controller-runtime/pkg/reconciler"
 	"github.com/matrixorigin/matrixone-operator/api/core/v1alpha1"
@@ -23,7 +25,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
-	"time"
 )
 
 const (
@@ -37,6 +38,7 @@ func (r *Actor) migrate(ctx *recon.Context[*v1alpha1.CNClaim]) error {
 	if c.Spec.SourcePod == nil {
 		return r.completeMigration(ctx)
 	}
+	ctx.Log.Info("migrate claimed pod", "source", c.Spec.SourcePod.PodName, "target", c.Spec.PodName)
 	source := &corev1.Pod{}
 	if err := ctx.Get(types.NamespacedName{Namespace: c.Namespace, Name: c.Spec.SourcePod.PodName}, source); err != nil {
 		if apierrors.IsNotFound(err) {
@@ -46,7 +48,7 @@ func (r *Actor) migrate(ctx *recon.Context[*v1alpha1.CNClaim]) error {
 	if c.Status.Store.BoundTime == nil {
 		return errors.New(fmt.Sprintf("claim store %s/%s bound time is nil", c.Namespace, c.Name))
 	}
-	if time.Now().Sub(c.Status.Store.BoundTime.Time) < propagationDelay {
+	if time.Since(c.Status.Store.BoundTime.Time) < propagationDelay {
 		return recon.ErrReSync("target pod is not ready to accept traffic, delay", propagationDelay)
 	}
 	switch source.Labels[v1alpha1.CNPodPhaseLabel] {
@@ -57,6 +59,7 @@ func (r *Actor) migrate(ctx *recon.Context[*v1alpha1.CNClaim]) error {
 		return recon.ErrReSync("source pod is still draining, requeue", migrationResyncInterval)
 	case v1alpha1.CNPodPhaseBound:
 		// use connection migration to migrate workload from source to target pod
+		ctx.Log.Info("start draining source pod", "pod", source.Name)
 		if err := r.reclaimCN(ctx, source, deleteOnReclaim); err != nil {
 			return err
 		}
@@ -64,8 +67,11 @@ func (r *Actor) migrate(ctx *recon.Context[*v1alpha1.CNClaim]) error {
 			return err
 		}
 		return recon.ErrReSync("source pod start draining, reqeue", migrationResyncInterval)
+	case v1alpha1.CNPodPhaseTerminating:
+		return r.completeMigration(ctx)
+	default:
+		return errors.Errorf("unknown pod phase: %s", source.Labels[v1alpha1.CNPodPhaseLabel])
 	}
-	return r.completeMigration(ctx)
 }
 
 func (r *Actor) reportProgress(ctx *recon.Context[*v1alpha1.CNClaim], source *corev1.Pod) error {
@@ -90,17 +96,13 @@ func (r *Actor) reportProgress(ctx *recon.Context[*v1alpha1.CNClaim], source *co
 
 func (r *Actor) completeMigration(ctx *recon.Context[*v1alpha1.CNClaim]) error {
 	c := ctx.Obj
-	if err := ctx.PatchStatus(c, func() error {
-		c.Status.Migrate = nil
-		return nil
-	}); err != nil {
-		return err
+	c.Status.Migrate = nil
+	if err := ctx.UpdateStatus(c); err != nil {
+		return errors.Wrap(err, 0)
 	}
-	if err := ctx.Patch(c, func() error {
-		c.Spec.SourcePod = nil
-		return nil
-	}); err != nil {
-		return err
+	c.Spec.SourcePod = nil
+	if err := ctx.Update(c); err != nil {
+		return errors.Wrap(err, 0)
 	}
 	return nil
 }
